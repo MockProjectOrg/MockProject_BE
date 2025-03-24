fcd261ce9cb727117af1f080ba41b66f999d0faa
ADMIN <ADMIN@MSI>
2025-03-24 19:14:45 +0700

    up

--- Booking-BE/src/main/java/org/example/bookingbe/service/ImageService/IImageService.java
+++ Booking-BE/src/main/java/org/example/bookingbe/service/ImageService/IImageService.java
@@ -1,4 +1,7 @@
 package org.example.bookingbe.service.ImageService;
 
+import java.util.List;
+
 public interface IImageService {
+    List<String> getImagesByRoomId(Long roomId);
 }
--- Booking-BE/src/main/java/org/example/bookingbe/service/ImageService/ImageService.java
+++ Booking-BE/src/main/java/org/example/bookingbe/service/ImageService/ImageService.java
@@ -1,7 +1,27 @@
 package org.example.bookingbe.service.ImageService;
 
+import org.example.bookingbe.model.Room;
+import org.example.bookingbe.repository.ImageRepo.IImageRepo;
+import org.example.bookingbe.repository.RoomRepo.IRoomRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ImageService implements IImageService {
+
+    @Autowired
+    private IImageRepo imageRepo;
+
+    @Autowired
+    private IRoomRepo roomRepo;
+
+    @Override
+    public List<String> getImagesByRoomId(Long roomId) {
+        Room room = roomRepo.findById(roomId).orElse(null);
+        if (room == null) return List.of();  // Trả về danh sách rỗng nếu không tìm thấy phòng
+
+        return imageRepo.findImagesByRoom(room);
+    }
 }
+
